f43939a9cd2617c0b7a2b70597a9861ef0b32fd3
[Copelia] <iamcorallium@gmail.com>
2018-10-02 02:07:42 -0500

    Refactorizando, fotos no jalan

---
+++ src/components/Dog.vue
@@ -0,0 +1,23 @@
+<template>
+<v-card color="grey lighten-2">
+<v-img :src="dog.imgn" height="170px">
+</v-img>
+  <v-card-title>
+    <div>
+      <h3>{{dog.name}}</h3>
+      <p>{{dog.breed}}</p>
+    </div>
+  </v-card-title>
+</v-card>
+
+</template>
+
+<script>
+export default {
+    props: {
+	    dog: {
+	      type: Object
+	    }
+	  }
+};
+</script>
--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -1,22 +1,29 @@
 <template>
   <v-container grid-list-md fluid>
     <v-layout wrap>
-      <v-flex xs12 sm4 md3>
-        <v-card color="grey lighten-2">
-          <v-img src="https://goo.gl/6CQNDo" height="170px">
-          </v-img>
-          <v-card-title>
-            <div>
-              <h3>Looking for a dog?</h3>
-            </div>
-          </v-card-title>
-        </v-card>
+      <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
+           <app-dog :dog="pet"></app-dog>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
-<script>import {Dogs} from "../data/dogs";</script>
+<script>
+import {Dogs} from "../data/dogs";
+import Dog from '../components/Dog.vue';
+
+  export default {
+      components: {
+    appDog: Dog,
+  },
+    data() {
+      return {
+        dogs: Dogs
+      };
+    }
+   };
+
+</script>
 
 <style scoped>
 p {
